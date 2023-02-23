6221478de72a49a0ac793605263fc51135e1fbd2
Bruna Oliveira <oliveira_bruna@outlook.com>
2023-02-23 11:33:08 -0300

    fix: Correções menu e slide card

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,43 +4,47 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 // menu
 
-function mudarTamanho(){
-  if(window.innerWidth >= 768){
-    nav.style.display = "flex"; 
-  }else{
+function mudarTamanho() {
+  if (window.innerWidth >= 768) {
+    nav.style.display = "flex";
+  } else {
     nav.style.display = "none";
   }
 }
 
-function clickMenu(){
-  if(nav.style.display == "block"){
+function clickMenu() {
+  if (nav.style.display == "block") {
     nav.style.display = "none";
-  }else{
+  } else {
     nav.style.display = "block";
   }
 }
